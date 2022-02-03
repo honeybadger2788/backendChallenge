7a40b4e3c40a812f732bcc3a86d677f5d2c59bbb
honeybadger2788 <noe.carosella@gmail.com>
2022-02-03 13:14:48 -0300

    create users router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
+const usersRouter = require('./routes/users.routes/users.router')
+
 let port = process.env.PORT || 3000;
 
-app.use('/', (req, res) => {
- res.send("hello world")   
-});
+app.use('/', usersRouter);
 
 app.listen(port, function() {
     console.log(`El servidor está corriendo en el puerto: ${port}`)
