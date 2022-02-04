33dd65776c23dea4cc72d5f212c13b221f94f6a3
honeybadger2788 <noe.carosella@gmail.com>
2022-02-04 12:52:05 -0300

    add movies model

---
+++ src/database/models/Movies.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Movie";
+
+    let cols = {
+        id_movie: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            notNull: true
+        },
+        image_url: {
+            type: dataTypes.STRING(45),
+            notNull: true
+        },
+        title: {
+            type: dataTypes.STRING(45),
+            notNull: true
+        },
+        creation_date: {
+            type: dataTypes.DATE,
+            notNull: true
+        },
+        rate: {
+            type: dataTypes.INTEGER,
+            notNull: true
+        },
+        id_genre: {
+            type: dataTypes.INTEGER,
+            notNull: true
+        },
+    }
+    let config = {
+        tableName: 'movies',
+        timestamps: true,
+        underscored: true
+    }
+       
+    const Movie = sequelize.define (alias, cols, config);
+    
+    return Movie
+}
