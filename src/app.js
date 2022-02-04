f8cfa31f020bb86ff754e940d47ec91ec7c38612
honeybadger2788 <noe.carosella@gmail.com>
2022-02-04 12:43:03 -0300

    add movies router

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 
 const usersRouter = require('./routes/users.routes/users.router')
+const moviesRouter = require('./routes/movies.routes/movies.router')
 
 let port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/user', usersRouter);
+app.use('/users', usersRouter);
+
+app.use('/movies', moviesRouter);
 
 app.listen(port, function() {
     console.log(`El servidor está corriendo en el puerto: ${port}`)
