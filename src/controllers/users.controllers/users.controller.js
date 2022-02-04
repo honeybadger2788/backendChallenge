983a095137a8fd2f1be2ed5358cc22b2339b23ae
honeybadger2788 <noe.carosella@gmail.com>
2022-02-04 11:53:13 -0300

    add user login

--- src/controllers/users.controllers/users.controller.js
+++ src/controllers/users.controllers/users.controller.js
@@ -14,7 +14,27 @@ module.exports = {
             }
         })
         .then(result => {
-            result[1] ? res.json({ status: 200, token: result[0].token}) : res.json({ status: 400, body: 'Usuario no creado'})
+            result[1] ?
+            res.json({ status: 201, token: result[0].token }) :
+            res.json({ status: 412, body: 'Usuario ya registrado' })
+        })
+        .catch(e => {
+            res.json({
+                status: 500,
+                body: e
+            })
+        })
+    },
+    login: (req,res) => {
+        db.User.findOne({
+            where:{
+                username: req.body.username
+            } 
+        })
+        .then(result => {
+            result && result.password === req.body.password && result.token === req.body.token ?
+            res.json({ status: 200, body: 'Ok' }) :
+            res.json({ status: 404, body: 'Usuario y/o contraseña incorrecta' })
         })
         .catch(e => {
             res.json({
--- src/routes/users.routes/users.router.js
+++ src/routes/users.routes/users.router.js
@@ -4,5 +4,6 @@ const router = Router();
 const usersController = require('../../controllers/users.controllers/users.controller')
 
 router.post('/auth/register', usersController.register);
+router.post('/auth/login', usersController.login);
 
 module.exports = router
