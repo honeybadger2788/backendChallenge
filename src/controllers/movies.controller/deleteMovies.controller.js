9f34eee9250848b9b58cbe000dff5518c3b05929
honeybadger2788 <noe.carosella@gmail.com>
2022-02-10 14:55:52 -0300

    add delete movies

--- src/controllers/movies.controller/deleteMovies.controller.js
+++ src/controllers/movies.controller/deleteMovies.controller.js
@@ -1,22 +1,16 @@
 const db = require('../../database/models/index');
 
 module.exports = (req,res) => {
-    const { title, image_url, launch_date, rate, id_genre } = req.body
-    db.Movie.update({
-        title,
-        image_url,
-        launch_date,
-        rate,
-        id_genre
-    },
+    const { title } = req.body
+    db.Movie.destroy(
     {
         where: {
             title
         }
     })
     .then(result => {
-        result[0] === 1 ?
-        res.json({ status: 200, body: 'Pelicula actualizada exitosamente' }) :
+        result[0] === 1?
+        res.json({ status: 200, body: 'Pelicula eliminada exitosamente' }) :
         res.json({ status: 404, body: 'Pelicula no encontrada' })
     })
     .catch(e => {
--- src/routes/movies.routes/movies.router.js
+++ src/routes/movies.routes/movies.router.js
@@ -4,9 +4,11 @@ const router = Router();
 const getMovies = require('../../controllers/movies.controller/getMovies.controller')
 const createMovies = require('../../controllers/movies.controller/createMovies.controller')
 const updateMovies = require('../../controllers/movies.controller/updateMovies.controller')
+const deleteMovies = require('../../controllers/movies.controller/deleteMovies.controller')
 
 router.get('/', getMovies)
 router.post('/', createMovies)
 router.put('/', updateMovies)
+router.delete('/', deleteMovies)
 
 module.exports = router
