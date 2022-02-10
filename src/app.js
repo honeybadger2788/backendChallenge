a7489d4e5400d46734952d8bb8f0e4677e771f59
honeybadger2788 <noe.carosella@gmail.com>
2022-02-10 15:10:45 -0300

    create character router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const usersRouter = require('./routes/users.routes/users.router')
 const moviesRouter = require('./routes/movies.routes/movies.router')
+const charactersRouter = require('./routes/characters.routes/characters.router')
 
 let port = process.env.PORT || 3000;
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use('/users', usersRouter);
 
 app.use('/movies', moviesRouter);
+
+app.use('/characters', charactersRouter);
 
 app.listen(port, function() {
     console.log(`El servidor está corriendo en el puerto: ${port}`)
