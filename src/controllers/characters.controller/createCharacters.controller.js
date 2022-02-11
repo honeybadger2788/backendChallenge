36ff265965c05491e2b1f57b51e7efc2f96bd891
honeybadger2788 <noe.carosella@gmail.com>
2022-02-11 18:00:44 -0300

    validate movies and characters created at createCharacters

--- src/controllers/characters.controller/createCharacters.controller.js
+++ src/controllers/characters.controller/createCharacters.controller.js
@@ -3,24 +3,29 @@ const db = require('../../database/models/index');
 module.exports = async (req, res) => {
     const { name, image_url, movies, age, weight, story } = req.body
     
-    const character = await db.Character.create({
-        name,
-        image_url,
-        age,
-        weight,
-        story,
+    const characterCreated = await db.Character.findOrCreate({
+        where: {name},
+        defaults: {
+            image_url,
+            age,
+            weight,
+            story
+        }
     })
     
     for (let i = 0; i < movies.length; i++) {
         const { title, image_url, launch_date, rate, id_genre } = movies[i]
-        const movie = await db.Movie.create({
-            title,
-            image_url,
-            launch_date,
-            rate,
-            id_genre
+        const movieCreated = await db.Movie.findOrCreate({
+            where: { title },
+            defaults: {
+                image_url,
+                launch_date,
+                rate,
+                id_genre
+            }
         })
-        await character.addMovie(movie)
+        if(movieCreated[1])
+            await characterCreated[0].addMovie(movieCreated[0])
     }
     
     const result = await db.Character.findOne({
