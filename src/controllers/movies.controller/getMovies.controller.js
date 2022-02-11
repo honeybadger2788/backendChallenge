b9b25e2c86050e61b7b33ae3d6dcb0cc7bee52e8
honeybadger2788 <noe.carosella@gmail.com>
2022-02-11 11:59:37 -0300

    fix filters in getMovies

--- src/controllers/movies.controller/getMovies.controller.js
+++ src/controllers/movies.controller/getMovies.controller.js
@@ -1,10 +1,25 @@
 const db = require('../../database/models/index');
 
-module.exports = (req, res) => { 
-    db.Movie.findAll({
-        attributes: [ 'title', 'image_url', 'launch_date' ],
-        where: req.query
+module.exports = (req, res) => {
+    const { order } = req.query
+    
+    let filters = {}
+    let query = { attributes: [ 'title', 'image_url', 'launch_date' ] }
+    
+    const filterOptions = ['title', 'id_genre']
+    const field = 'launch_date'
+    const validSortingTypes = ['asc', 'desc']
+    
+    Object.keys(req.query).forEach(key => {
+        if(filterOptions.includes(key))
+            filters = { ...filters, [key]: req.query[key] }
+            query = { ...query, where: filters }
     })
+    
+    if (order != null && validSortingTypes.includes(order.toLowerCase()))
+        query = { ...query, order: [[field, order]] }
+    
+    db.Movie.findAll(query)
     .then(result => {
         res.json({ status: 200, body: result })
     })
