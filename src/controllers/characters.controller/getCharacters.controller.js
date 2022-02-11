c4fb0b57ed0ea1a06e91ac4e3caeb9de2fa3c2bc
honeybadger2788 <noe.carosella@gmail.com>
2022-02-11 12:25:36 -0300

    add filters options in getCharacters

--- src/controllers/characters.controller/getCharacters.controller.js
+++ src/controllers/characters.controller/getCharacters.controller.js
@@ -1,14 +1,25 @@
 const db = require('../../database/models/index');
 
-module.exports = (req, res) => {
-    db.Character.findAll({
-        attributes: [ 'name', 'image_url' ],
-        where: {
-            deleted_at: null
-        }
+module.exports = (req, res) => { 
+    let filters = {}
+    // permite armar la query de busqueda dinamica
+    let query = { attributes: ['name', 'image_url'] } // campos a mostrar
+    
+    // falta agregar el filtro por serie o peliculas
+    const filterOptions = ['name', 'age', 'weight'] // filtros admitidos
+    
+    // valida que los filtros ingresados esten entre los admitidos
+    Object.keys(req.query).forEach(key => {
+        if(filterOptions.includes(key))
+            filters = { ...filters, [key]: req.query[key] } // permite insertar varios filtros
+            query = { ...query, where: filters }
     })
+
+    db.Character.findAll(query)
     .then(result => {
-        res.json({ status: 200, body: result })
+        result.length !== 0 ?
+            res.json({ status: 200, body: result }) : 
+            res.json({ status: 404, body: 'Personaje/s no encontrado/s' })
     })
     .catch(e => {
         res.json({
