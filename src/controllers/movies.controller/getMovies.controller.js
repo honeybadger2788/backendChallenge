efaf0fb739df6748e9678b9b83c89695b3dec770
honeybadger2788 <noe.carosella@gmail.com>
2022-02-11 12:08:08 -0300

    add comments in getMovies and status 404 response

--- src/controllers/movies.controller/getMovies.controller.js
+++ src/controllers/movies.controller/getMovies.controller.js
@@ -4,24 +4,29 @@ module.exports = (req, res) => {
     const { order } = req.query
     
     let filters = {}
-    let query = { attributes: [ 'title', 'image_url', 'launch_date' ] }
+    // permite armar la query de busqueda dinamica
+    let query = { attributes: [ 'title', 'image_url', 'launch_date' ] } // campos a mostrar
     
-    const filterOptions = ['title', 'id_genre']
-    const field = 'launch_date'
-    const validSortingTypes = ['asc', 'desc']
+    const filterOptions = ['title', 'id_genre'] // filtros admitidos
+    const field = 'launch_date' // campo de ordenamiento admitido
+    const validSortingTypes = ['asc', 'desc'] // ordenamiento admitido
     
+    // valida que los filtros ingresados esten entre los admitidos
     Object.keys(req.query).forEach(key => {
         if(filterOptions.includes(key))
-            filters = { ...filters, [key]: req.query[key] }
+            filters = { ...filters, [key]: req.query[key] } // permite insertar varios filtros
             query = { ...query, where: filters }
     })
     
+    // inserta la regla de ordenamiento solo en caso de haberla ingresado
     if (order != null && validSortingTypes.includes(order.toLowerCase()))
         query = { ...query, order: [[field, order]] }
     
     db.Movie.findAll(query)
     .then(result => {
-        res.json({ status: 200, body: result })
+        result.length !== 0 ?
+            res.json({ status: 200, body: result }) : 
+            res.json({ status: 404, body: 'Pelicula/s no encontrada/s' })
     })
     .catch(e => {
         res.json({
