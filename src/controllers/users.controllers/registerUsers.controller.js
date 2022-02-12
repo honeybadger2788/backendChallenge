b0aeab4044983ee6e4465f05eb7e793affbafa21
honeybadger2788 <noe.carosella@gmail.com>
2022-02-12 16:14:06 -0300

    change promises by async/await at registerUsers controller

--- src/controllers/users.controllers/registerUsers.controller.js
+++ src/controllers/users.controllers/registerUsers.controller.js
@@ -2,25 +2,44 @@ const db = require('../../database/models/index');
 
 const generateApiKey = require('generate-api-key');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+    
     const { username } = req.body
-    db.User.findOrCreate({
-        where: {
-            username
-        },
-        defaults: {
-            token: generateApiKey({ method: 'string', length: 45 }),
-        }
+    
+    if (!username)
+    return res.json({
+        status: 400,
+        msg: 'Ingresar username'
     })
-    .then(result => {
-        result[1] ?
-        res.json({ status: 201, token: result[0].token }) :
-        res.json({ status: 412, body: 'Usuario ya registrado' })
-    })
-    .catch(e => {
-        res.json({
-            status: 500,
-            body: e
+    
+    try {
+
+        const result = await db.User.findOrCreate({
+            where: {
+                username
+            },
+            defaults: {
+                token: generateApiKey({ method: 'string', length: 45 }),
+            }
         })
-    })
+        
+        return result[1] ?
+        res.json({
+            status: 201,
+            msg: 'Usuario registrado exitosamente',
+            token: result[0].token
+        }) :
+        res.json({
+            status: 400,
+            body: 'Usuario ya registrado'
+        })
+        
+    } catch (e) {
+        
+        return res.json({
+            status: 500,
+            error: e
+        })
+        
+    }
 }
