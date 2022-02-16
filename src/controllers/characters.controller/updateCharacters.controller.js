6add166a318832b979801f6902c6655ce5e242ad
honeybadger2788 <noe.carosella@gmail.com>
2022-02-16 14:58:05 -0300

    change promises by async/await at updateCharacter controller

--- src/controllers/characters.controller/updateCharacters.controller.js
+++ src/controllers/characters.controller/updateCharacters.controller.js
@@ -1,27 +1,54 @@
 const db = require('../../database/models/index');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const { id_character } = req.params
     const { name, image_url, age, weight, story } = req.body
-    db.Character.update({
-        name,
-        image_url,
-        age,
-        weight,
-        story
-    },
-    {
-        where: { id_character }
-    })
-    .then(result => {
-        result[0] === 1 ?
-        res.json({ status: 200, body: 'Personaje actualizado exitosamente' }) :
-        res.json({ status: 404, body: 'Personaje no encontrado' })
-    })
-    .catch(e => {
-        res.json({
-            status: 500,
-            body: e
+
+    if (!name &&
+        !image_url &&
+        !age &&
+        !weight &&
+        !story)
+        return res.json({
+            error: {
+                status: 400,
+                msg: 'Ingresar datos a actualizar'
+            }
         })
-    })
+    
+    try {
+        
+        const result = await db.Character.update({
+            name,
+            image_url,
+            age,
+            weight,
+            story
+        },
+        {
+            where: { id_character }
+        })
+        
+        return result[0] === 1 ?
+        res.json({
+            status: 200,
+            msg: 'Personaje actualizado exitosamente'
+        }) :
+        res.json({
+            error: {
+                status: 404,
+                msg: 'Personaje no encontrado'
+            }
+        })
+        
+    } catch (e) {
+
+        res.json({
+            error: {
+                status: 500,
+                msg: e
+            }
+        })
+    }
+    
 }
