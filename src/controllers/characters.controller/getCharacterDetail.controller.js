e16aabb1a8f6d755b232a59be1c5899a2036401c
honeybadger2788 <noe.carosella@gmail.com>
2022-02-16 15:04:30 -0300

    change promises by async/await at getCharacterDetail controller

--- src/controllers/characters.controller/getCharacterDetail.controller.js
+++ src/controllers/characters.controller/getCharacterDetail.controller.js
@@ -3,11 +3,32 @@ const db = require('../../database/models/index');
 module.exports = async (req, res) => {
     const { id_character } = req.params
 
-    const result = await db.Character.findByPk(id_character, {
-        include: db.Movie
-    })
-    
-    result !== null ?
-        res.json({ status: 200, body: result }) : 
-        res.json({ status: 404, body: 'Personaje/s no encontrado/s' })
+    try {
+
+        const result = await db.Character.findByPk(id_character, {
+            include: db.Movie
+        })
+        
+        return result !== null ?
+            res.json({
+                status: 200,
+                data: result
+            }) : 
+            res.json({
+                error: {
+                    status: 404,
+                    msg: 'Personaje/s no encontrado/s'
+                }
+            })
+        
+    } catch (e) {
+
+        res.json({
+            error: {
+                status: 404,
+                msg: e
+            }
+        })
+
+    }
 }
