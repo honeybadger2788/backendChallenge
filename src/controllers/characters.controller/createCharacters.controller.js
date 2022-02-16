d9a3e6394c74c770aaafc45c6d9c97dc71f66309
honeybadger2788 <noe.carosella@gmail.com>
2022-02-16 15:22:20 -0300

    change promises by async/await at createCharacters controller

--- src/controllers/characters.controller/createCharacters.controller.js
+++ src/controllers/characters.controller/createCharacters.controller.js
@@ -3,35 +3,58 @@ const db = require('../../database/models/index');
 module.exports = async (req, res) => {
     const { name, image_url, movies, age, weight, story } = req.body
     
-    const characterCreated = await db.Character.findOrCreate({
-        where: {name},
-        defaults: {
-            image_url,
-            age,
-            weight,
-            story
-        }
-    })
-    
-    for (let i = 0; i < movies.length; i++) {
-        const { title, image_url, launch_date, rate, id_genre } = movies[i]
-        const movieCreated = await db.Movie.findOrCreate({
-            where: { title },
+    try {
+        
+        const characterCreated = await db.Character.findOrCreate({
+            where: {name},
             defaults: {
                 image_url,
-                launch_date,
-                rate,
-                id_genre
+                age,
+                weight,
+                story
             }
         })
-        if(movieCreated[1])
+        
+        for (let i = 0; i < movies.length; i++) {
+            const { title, image_url, launch_date, rate, id_genre } = movies[i]
+            const movieCreated = await db.Movie.findOrCreate({
+                where: { title },
+                defaults: {
+                    image_url,
+                    launch_date,
+                    rate,
+                    id_genre
+                }
+            })
+            if(movieCreated[1])
             await characterCreated[0].addMovie(movieCreated[0])
+        }
+        
+        const result = await db.Character.findOne({
+            where: { name },
+            include: db.Movie
+        })
+        
+        return characterCreated && result ?
+        res.json({
+            status: 201,
+            data: result
+        }) :
+        res.json({
+            error: {
+                status: 400,
+                msg: 'No se pudo crear el personaje'
+            }
+        })
+        
+    } catch (e) {
+        
+        res.json({
+            error: {
+                status: 500,
+                msg: e
+            }
+        })
     }
     
-    const result = await db.Character.findOne({
-        where: { name },
-        include: db.Movie
-    })
-    
-    res.json({ status: 201, body: result })
 }
